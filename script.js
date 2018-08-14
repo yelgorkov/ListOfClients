e1bb4d72a35e41d74d7db18cf440ab0b8130d1ba
yel.gorkov <yel.gorkov@gmail.com>
2018-08-14 23:53:53 +0300

    added new functionality

--- script.js
+++ script.js
@@ -24,5 +24,52 @@ function addNewClients(student, index, arr) {
 	newParagraph.appendChild(text);
 	newClient.appendChild(newImage);
 	newClient.appendChild(newParagraph);
-	document.body.appendChild(newClient);
+	const menu = document.getElementById('clients-menu');
+	menu.appendChild(newClient);
+
+	newClient.addEventListener('click', function() {
+ 		
+
+		const fullDiscription = document.getElementById('box');
+		const temporaryDiv = document.createElement('div');
+		const myEle = document.getElementById("temp");
+		temporaryDiv.setAttribute("id", "temp");
+
+		if (myEle) {
+			fullDiscription.removeChild(myEle);
+		}
+
+		const client = document.createElement('p');
+		const job = document.createElement('p');
+		const contact = document.createElement('p');
+		const address = document.createElement('p');
+		const bigImage = document.createElement('img');
+
+
+		fullDiscription.appendChild(temporaryDiv);
+		temporaryDiv.appendChild(bigImage);
+		temporaryDiv.appendChild(client);
+		temporaryDiv.appendChild(job);
+		temporaryDiv.appendChild(contact);
+		temporaryDiv.appendChild(address);
+
+		const clientText = document.createTextNode(student.general.firstName + ' ' + student.general.lastName);
+		const jobText = document.createTextNode('Job: ' + student.job.company + '. Position: ' + student.job.title);
+		const contactText = document.createTextNode('Contacts: ' + student.contact.email + ', ' + student.contact.phone);
+		const adressText = document.createTextNode('Address: ' + student.address.street + ', ' + student.address.city + ', ' + student.address.zipCode + ', ' + student.address.country);
+
+		bigImage.src = student.general.avatar;
+
+		client.appendChild(clientText);
+		job.appendChild(jobText);
+		contact.appendChild(contactText);
+		address.appendChild(adressText);
+
+	})
+	newClient.addEventListener('mouseover', function() {
+		this.style.background = 'grey';
+	})
+	newClient.addEventListener('mouseout', function() {
+		this.style.background = 'blue';
+	})
 }
